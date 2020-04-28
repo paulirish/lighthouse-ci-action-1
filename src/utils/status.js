92396dfad9b5ee62991a5ed830ce08b6daa560b0
Paul Irish <commits@paul.irish>
2020-04-27 18:55:52 -0700

    edit

--- src/utils/status.js
+++ src/utils/status.js
@@ -5,18 +5,24 @@ async function postStatus({sha}) {
 
   const token = core.getInput('secretghtoken');
 
+  const statusPayload = {
+    state: 'pending',
+    target_url: ' https://lhci-canary.herokuapp.com/app/projects/debugger-protocol-viewer/compare/b9fac0a9-7024-45ae-b4ee-854fd089f460?compareUrl=http%3A%2F%2Flocalhost%3APORT%2Fdevtools-protocol%2Ftot%2FPage%2F',
+    description: 'What a great thing',
+    context: 'LHCI server',
+  };
+
+  const status_url = `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/statuses/${sha}`;
+
+  console.log({statusPayload, status_url});
+
   // https://developer.github.com/v3/repos/statuses/#create-a-status
-  const response = await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/statuses/${sha}`, {
+  const response = await fetch(status_url, {
     method: 'POST',
     headers: {
       'Authorization': `token ${token}`,
     },
-    body: JSON.stringify({
-      state: 'pending',
-      target_url: ' https://lhci-canary.herokuapp.com/app/projects/debugger-protocol-viewer/compare/b9fac0a9-7024-45ae-b4ee-854fd089f460?compareUrl=http%3A%2F%2Flocalhost%3APORT%2Fdevtools-protocol%2Ftot%2FPage%2F',
-      description: 'What a great thing',
-      context: 'LHCI server',
-    }),
+    body: JSON.stringify(statusPayload),
   });
 
   console.log({response});
