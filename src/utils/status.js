5fb87e0b1ad20d071d2b1920b54ac35438148075
Paul Irish <commits@paul.irish>
2020-04-27 18:51:13 -0700

    maybe this does something

--- src/utils/status.js
+++ src/utils/status.js
@@ -1,25 +1,33 @@
 const fetch = require('node-fetch')
+const core = require('@actions/core')
 
-function postStatus() {
+async function postStatus({sha}) {
+
+  const token = core.getInput('secretghtoken');
+
   // https://developer.github.com/v3/repos/statuses/#create-a-status
-  fetch(`https://api.github.com/repos/unsplash/unsplash-web/statuses/${sha}`, {
+  const response = await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/statuses/${sha}`, {
     method: 'POST',
     headers: {
-      [requestHeaders.AUTHORIZATION_REQUEST_HEADER]: `token ${buildTrackerGithubToken}`,
-      [requestHeaders.CONTENT_TYPE_REQUEST_HEADER]: ContentType.Json,
+      'Authorization': `token ${token}`,
     },
     body: JSON.stringify({
-      state,
-      target_url: targetUrl,
-      description,
-      context: 'Build Tracker',
+      state: 'pending',
+      target_url: ' https://lhci-canary.herokuapp.com/app/projects/debugger-protocol-viewer/compare/b9fac0a9-7024-45ae-b4ee-854fd089f460?compareUrl=http%3A%2F%2Flocalhost%3APORT%2Fdevtools-protocol%2Ftot%2FPage%2F',
+      description: 'What a great thing',
+      context: 'LHCI server',
     }),
-  }).then((response) =>
-    response.ok === false
-      ? response.json().then((json) => {
-          throw new Error(`Response was not ok. Status: ${response.status}. Body: ${JSON.stringify(json)}`)
-        })
-      : Promise.resolve()
-  )
+  });
+
+  console.log({response});
+
+  // .then((response) =>
+  //   response.ok === false
+  //     ? response.json().then((json) => {
+  //         throw new Error(`Response was not ok. Status: ${response.status}. Body: ${JSON.stringify(json)}`)
+  //       })
+  //     : Promise.resolve()
+  // )
+  return;
 }
 module.exports = { postStatus }
